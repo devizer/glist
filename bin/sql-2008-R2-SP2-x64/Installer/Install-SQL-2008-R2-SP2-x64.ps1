424f8d9620f8395441a941248ece70074fb3bb69
Volodymyr Hoida <devizer@users.github.com>
2017-11-02 01:48:31 +0300

    SQL 2008 R2 (Express) Installer is done
    Former-commit-id: c1efdd7b1ee271c427d5ed7dd36884e59de0ef78

--- bin/sql-2008-R2-SP2-x64/Installer/Install-SQL-2008-R2-SP2-x64.ps1
+++ bin/sql-2008-R2-SP2-x64/Installer/Install-SQL-2008-R2-SP2-x64.ps1
@@ -18,6 +18,7 @@ $pars=@($Temp)
 foreach($part in @(1,2,3)) { 
    $pars += "https://github.com/devizer/glist/raw/master/bin/sql-2008-R2-SP2-x64/SQLEXPR-x86-ENU-2008-R2-SP2.7z.exe.7z.00$part"; 
 }
+
 pushd $Temp
 cmd /c "$Essentials\Parallel-Download.exe" $pars
 cmd /c "$Essentials\x86\7z.exe" @("x", "-y", "SQLEXPR-x86-ENU-2008-R2-SP2.7z.exe.7z.001")
@@ -27,7 +28,7 @@ popd
 
 Write-Host "Installing .NET 3.5 and 4.5"
 Add-WindowsFeature Net-Framework-Core
-Add-WindowsFeature NET-Framework-45-Features
+Add-WindowsFeature NET-Framework-45-Core
 pushd $Env:windir\microsoft.net
 Framework64\v2.0.50727\ngen.exe  queue pause
 Framework64\v4.0.30319\ngen.exe  queue pause
@@ -35,5 +36,11 @@ Framework\v2.0.50727\ngen.exe    queue pause
 Framework\v4.0.30319\ngen.exe    queue pause
 popd
 
-$target="C:\SQL\2008-R2-x86-Express"
-cmd /c setup.exe /Action=Install /ADDCURRENTUSERASSQLADMIN /IACCEPTSQLSERVERLICENSETERMS /ENU /FEATURES=SQLENGINE /INSTANCENAME=SQL2008R2 /SECURITYMODE=SQL /SAPWD=`1qazxsw2 /TCPENABLED=1 /NPENABLED=1 /INDICATEPROGRESS /QS /INSTANCEDIR="$target"
+$target="C:\SQL"
+pushd "$temp\SQLEXPR-x86-ENU-2008-R2-SP2"
+cmd /c setup.exe /Action=Install /ADDCURRENTUSERASSQLADMIN /IACCEPTSQLSERVERLICENSETERMS /ENU /FEATURES=SQLENGINE /INSTANCENAME=SQL2008R2SP2 /SECURITYMODE=SQL /SAPWD=`1qazxsw2PasS /TCPENABLED=1 /NPENABLED=1 /INDICATEPROGRESS /QS /SQLSVCACCOUNT="NT AUTHORITY\SYSTEM" /INSTANCEDIR="$target" /SQLSYSADMINACCOUNTS="BUILTIN\ADMINISTRATORS"
+popd
+
+Remove-Item -Recurse -Force "$temp\SQLEXPR-x86-ENU-2008-R2-SP2"
+$exe="$target\MSSQL10_50.SQL2008R2SP2\MSSQL\Binn\sqlservr.exe"
+cmd /c netsh firewall add allowedprogram `"$exe`" `"SQL Express 2008 R2 SP2 x86`" ENABLE
