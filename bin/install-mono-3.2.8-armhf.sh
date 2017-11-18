2bc58ceae88e8dd53d35c0d31e17dc1f7edc7869
Volodymyr Hoida <devizer@users.github.com>
2017-11-18 04:51:32 +0300

    added mono 3.2.8 for armhf
    Former-commit-id: f63a4639fe614084eeab02d2b33272c2a4b19dfc

--- bin/install-mono-3.2.8-armhf.sh
+++ bin/install-mono-3.2.8-armhf.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 f=mono-3.2.8-armhf.tar.bz2
-(command -v curl >> /dev/null) && curl -o $f https://raw.githubusercontent.com/devizer/glist/master/bin/$f
-if [ ! -f $f ]; then
 (command -v wget >> /dev/null) && wget --no-check-certificate -O $f https://raw.githubusercontent.com/devizer/glist/master/bin/$f
+if [ ! -f $f ]; then
+(command -v curl >> /dev/null) && curl -o $f https://raw.githubusercontent.com/devizer/glist/master/bin/$f
 fi
-tar xjf $f -Ñ /
+tar xjf $f -C /
 rm $f
 /opt/mono/3.2.8/bin/mono --version
 echo '
@@ -15,3 +15,6 @@ export PATH="/opt/mono/$mver/bin:$PATH"
 export LD_LIBRARY_PATH="/opt/mono/$mver/lib:$LD_LIBRARY_PATH"
 export PKG_CONFIG_PATH="/opt/mono/$mver/lib/pkgconfig:$PKG_CONFIG_PATH"
 mono --version | head -1'
+
+# One Line Installer: 
+# wget -q -nv --no-check-certificate -O - https://raw.githubusercontent.com/devizer/glist/master/bin/install-mono-3.2.8-armhf.sh | bash
