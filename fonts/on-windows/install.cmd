c3383c1f8b2552cb2804b53aab2095574f23f67f
devizer <devizer@users.github.com>
2017-12-22 20:32:19 +0300

    Improovedd fonts installer.cmd (on windows)
    Former-commit-id: 51eda1e596053257f1a451f0c58a911815dc6e63

--- fonts/on-windows/install.cmd
+++ fonts/on-windows/install.cmd
@@ -1,22 +1,31 @@
 @echo off
 set work=%USERPROFILE%\Temp\Fonts
 mkdir "%work%" 1>nul 2>&1
+
+for %%f in (RegisterFont.exe RegisterFont.exe.config unzip.exe) DO (
+  set url=https://github.com/devizer/glist/raw/master/fonts/on-windows/%%f
+  set outfile=%work%\%%f
+  echo Downloading "%%f" into [%work%\%%f]
+  call :download
+)
+
+
 for %%z in (Roboto Fira OpenSans liberation ubuntu-font-family Google-SuperFamilies) DO (
   set url=https://github.com/devizer/glist/raw/master/fonts/%%z.zip
   set outfile=%work%\%%z.zip
   echo Downloading "%%z" into [%work%\%%z]
   call :download
   echo Extracting %%z zip
-  unzip -o "%work%\%%z.zip" -d "%work%\files"
+  "%work%\unzip.exe" -o "%work%\%%z.zip" -d "%work%\files"
 )
 
 :skip
 for /r "%work%\files" %%f in (*.ttf) DO (
   xcopy /y "%%f" %windir%\Fonts\*
-  RegisterFont.exe "%windir%\Fonts\%%~nf.ttf"
+  "%work%\RegisterFont.exe" "%windir%\Fonts\%%~nf.ttf"
 )
 rem echo "Notify all the running apps about now fonts"
-rem RegisterFont.exe ":broadcast"
+rem "%work%\RegisterFont.exe" ":broadcast"
 echo "DONE. Finish. Bye"
 
 goto exit;
