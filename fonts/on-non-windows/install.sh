e487f046592f4ac7d386cedbd9f515393ab61ff2
devizer <devizer@users.github.com>
2017-12-22 23:36:47 +0300

    Added fonts installer.sh (on non-windows)
    Former-commit-id: 637578671bb804d9f7e195bf8839cf0a3eead879

---
+++ fonts/on-non-windows/install.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/env bash
+cd /tmp
+for f in Roboto Fira OpenSans liberation ubuntu-font-family Google-SuperFamilies; do
+  echo $f
+  wget --no-check-certificate -O $f.zip https://raw.githubusercontent.com/devizer/glist/master/fonts/$f.zip
+  unzip -o $f.zip -d fontfiles/
+  rm -f $f
+done
+
+pushd fontfiles
+if [ -d "/usr/share/fonts" ]; then target=/usr/share/fonts; else target=/usr/local/share/fonts; fi
+for i in $(ls -d */); do 
+  dir=${i%%/}
+  echo Dir: $dir
+  sudo mkdir -p $target/truetype
+  sudo cp -rf "./$dir" $target/truetype/
+  rm -rf $dir
+done
+
+fc-cache -f -v
+
