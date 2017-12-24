1689ac805693f00d4fbcd143d817a7854513be65
Vladimir Goyda <vgoyda@gmail.com>
2017-12-24 16:41:08 +0300

    Fixed FontInstaller (on windows)
    Former-commit-id: 9cb96e7a698073ffb9a3a1b45e3a1aecb232433c

--- fonts/on-windows/RegisterFont.cs
+++ fonts/on-windows/RegisterFont.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.ComponentModel;
 using System.IO;
 using System.Runtime.InteropServices;
 
@@ -28,7 +29,16 @@ namespace RegisterFont
                     if (count == 0)
                         throw new Exception("None fonts added");
 
-                    WriteProfileString("fonts", Path.GetFileNameWithoutExtension(file) + "(TrueType)", file);
+                    var isOk = WriteProfileString("fonts", Path.GetFileNameWithoutExtension(file) + "(TrueType)", file);
+
+                    if (!isOk)
+                    {
+                        int errorCode = Marshal.GetLastWin32Error();
+                        throw new Exception("WriteProfileString failed. Win32 Error: "
+                            + errorCode
+                            + ". " + new Win32Exception(errorCode).Message);
+                    }
+
 
                     Console.WriteLine("Registered {0} font(s) using '{1}' file", count, file);
                     return 0;
@@ -47,7 +57,7 @@ namespace RegisterFont
         static IntPtr HWND_BROADCAST = new IntPtr(0xffff);
         
         [DllImport("kernel32.dll", SetLastError = true)]
-        static extern int WriteProfileString(string lpszSection, string lpszKeyName, string lpszString);
+        static extern bool WriteProfileString(string lpszSection, string lpszKeyName, string lpszString);
 
         [DllImport("user32.dll")]
         public static extern int SendMessage(IntPtr hWnd, uint msg, int wParam, int lParam);
