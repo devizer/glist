70819710c53401bc225a8e1a06b763437f8221ef
devizer <devizer@users.github.com>
2018-04-19 00:42:24 +0300

    Improved Install-SQL-2008-R2-SP2-x86.cmd
    Former-commit-id: 52230d72f93d6118202ede4969ef8b78b9417af2

--- bin/sql-2008-R2-SP2-x86/Installer/Install-SQL-2008-R2-SP2-x86.cmd
+++ bin/sql-2008-R2-SP2-x86/Installer/Install-SQL-2008-R2-SP2-x86.cmd
@@ -1,2 +1,7 @@
-powershell -File Install-SQL-2008-R2-SP2-x64.ps1
+rem powershell -File Install-SQL-2008-R2-SP2-x64.ps1
+set url=https://raw.githubusercontent.com/devizer/glist/master/bin/sql-2008-R2-SP2-x86/Installer/Install-SQL-2008-R2-SP2-x86.ps1
+set outfile=%AppData%\Temp\Install-SQL-2008-R2-SP2-x86.ps1
+mkdir "%AppData%\Temp" 1>nul 2>&1
+echo [System.Net.ServicePointManager]::ServerCertificateValidationCallback={$true}; $d=new-object System.Net.WebClient; $d.DownloadFile("$Env:url","$Env:outfile") | powershell -command -
+powershell -f "%outfile%"
 pause
