5aa500a201cace71378e7a3267c852c592db4c2a
devizer <devizer@users.github.com>
2018-05-09 15:26:48 +0300

    Fixed SQL Express Installers
    Former-commit-id: 3d141371fa260c38bddbb282fa3e8e7175656bf7

--- bin/SQL-Express/all.cmd
+++ bin/SQL-Express/all.cmd
@@ -5,3 +5,5 @@ call SQL-Express-2012-SP3.cmd
 call SQL-Express-2014-SP1.cmd
 rem  TODO: SQL 2016
 call SQL-Express-2017-Updated.cmd 
+
+echo get-wmiobject win32_service ^| where {$_.Name.ToLower().IndexOf("sql") -ge 0 } ^| ft Name, DisplayName, StartMode, State | powershell -c -
