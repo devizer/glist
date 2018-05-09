eaa9fd3aa328e1203c9a291a02d05b2906e56edd
devizer <devizer@users.github.com>
2018-05-09 16:34:03 +0300

    Added SQL 2008 SP3 x64 installer
    Former-commit-id: d638e0fdabdb8f57a13e12828f914453db645dd8

---
+++ bin/SQL-Express/SQL-Express-2008-SP3-x64.cmd
@@ -0,0 +1,25 @@
+@echo off
+
+set KEY=SQL-Express-2008-SP3-x64
+echo DOWNLOADING %KEY%.exe
+set url=https://download.microsoft.com/download/0/F/D/0FD88169-F86F-46E1-8B3B-56C44F6E9505/SQLEXPR_x64_ENU.exe
+set outfile=%AppData%\Temp\%KEY%.exe
+mkdir "%AppData%\Temp" 1>nul 2>&1
+echo [System.Net.ServicePointManager]::ServerCertificateValidationCallback={$true}; $d=new-object System.Net.WebClient; $d.DownloadFile("$Env:url","$Env:outfile") | powershell -command -
+
+echo Extracting %KEY%.exe
+@rem Suppress both GUI and "Extract completed" Message Box
+"%outfile%" /q /x:"%AppData%\Temp\%KEY%"
+del /F /Q "%outfile%"
+
+"%AppData%\Temp\%KEY%\Setup.exe" /QS /INDICATEPROGRESS /Action=Install ^
+  /ADDCURRENTUSERASSQLADMIN ^
+  /FEATURES=SQLENGINE ^
+  /INSTANCENAME=SQL2008SP3 ^
+  /SECURITYMODE=SQL /SAPWD=`1qazxsw2 ^
+  /SQLSVCACCOUNT="NT AUTHORITY\SYSTEM" ^
+  /INSTANCEDIR="%SystemDrive%\SQL" ^
+  /SQLSYSADMINACCOUNTS="BUILTIN\ADMINISTRATORS" ^
+  /TCPENABLED=1 /NPENABLED=1 
+
+rd /q /s "%AppData%\Temp\%KEY%"
