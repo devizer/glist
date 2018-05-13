d97169ac68aa62012536f5946d9b212401730bf0
devizer <devizer@users.github.com>
2018-05-14 00:16:54 +0300

    Fixed 7Zip @ Essentials
    Former-commit-id: f7e20df18be793b765c4c9c7c8f12e29a91ed2db

--- Essentials/build.cmd
+++ Essentials/build.cmd
@@ -3,8 +3,8 @@ pushd ..\Parallel-Download
 xcopy /y bin\Debug\*.* ..\Essentials\*.*
 popd
 
-del Essentials.7z 1>nul 2>nul
-C:\Apps\7-Zip-x86\7z a -t7z -mx=9 -mfb=128 -md=128m -ms=on -xr!Essentials.7z.exe Essentials.7z
-copy /b C:\Apps\7-Zip-x86\7zCon.sfx + Essentials.7z Essentials.7z.exe
-del Essentials.7z
+@del Essentials.7z 1>nul 2>nul
+x86\7z a -t7z -mx=9 -sfx7zCon.sfx -mfb=128 -md=128m -ms=on -xr!Essentials.7z.exe Essentials.7z.exe
+rem copy /b C:\Apps\7-Zip-x86\7zCon.sfx + Essentials.7z Essentials.7z.exe
+@del Essentials.7z 1>nul 2>nul
 del Parallel-Download.*
