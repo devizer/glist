3d82e8ff263166a13b4b4a974e021feb2e5de2a7
devizer <devizer@users.github.com>
2018-06-11 01:43:04 +0300

    Added SQL Express 2014 SP2 x86
    Former-commit-id: bdfdcada04b59acc0844df22c1584abb0b703e38

--- bin/SQL-Express/all.cmd
+++ bin/SQL-Express/all.cmd
@@ -1,6 +1,7 @@
 type sql-dev-2017.ps1 | powershell -c -
 call SQL-Express-2017-Updated.cmd 
 call SQL-Express-2016-Updated.cmd
+call SQL-Express-2014-SP2-x86.cmd
 call SQL-Express-2014-SP1.cmd
 call SQL-Express-2012-SP3.cmd
 call SQL-Express-2008-R2-SP2-x86.cmd
