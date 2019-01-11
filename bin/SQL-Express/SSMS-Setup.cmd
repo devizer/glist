6e255b1e3b23a8b6b08d500f92c38162206de2c3
devizer <devizer@users.github.com>
2019-01-11 10:03:17 +0200

    Updated DevTools installer
    Former-commit-id: 495274160c63b61a513e991a274ca270bc3dfb57

--- bin/SQL-Express/SSMS-Setup.cmd
+++ bin/SQL-Express/SSMS-Setup.cmd
@@ -1,5 +1,6 @@
 set KEY=SSMS-Setup
 echo DOWNLOADING SSMS-Setup
+rem 16.5.1 for x86: https://download.microsoft.com/download/9/3/3/933EA6DD-58C5-4B78-8BEC-2DF389C72BE0/SSMS-Setup-ENU.exe
 set url=https://download.microsoft.com/download/0/D/2/0D26856F-E602-4FB6-8F12-43D2559BDFE4/SSMS-Setup-ENU.exe
 set outfile=%AppData%\Temp\%KEY%.exe
 mkdir "%AppData%\Temp" 1>nul 2>&1
