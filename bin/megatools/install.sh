fd43caa7c2be4ab27de6ea1deed541f5ce3892a7
devizer <devizer@users.github.com>
2019-01-21 21:09:08 +0200

    added megatools for arm 32/64 and installer
    Former-commit-id: 8cce77bd3e0c8ba9fb2a1784a3bad2bdc905351c

--- bin/megatools/install.sh
+++ bin/megatools/install.sh
@@ -1,11 +1,18 @@
 #!/usr/bin/env bash
-if [[ $(uname -m) == armv7* ]]; then arch=arm32; else arch=arm64; fi; echo "ARM Architecture: $arch"
-url=https://raw.githubusercontent.com/devizer/glist/master/bin/megatools/megatools-$arch.tar.gz
-$work=$(mktemp -d mega-XXXXXXXX)
-pushd $work
-wget --nocheck-certificate -O _mega.tar.gz $url
-tar xzf _mega.tar.gz
-cd megatools*
-./megatools --version
-popd
+if [ "$(command -v megatools)" == "" ]; then
+  echo "Installing megatools for ARMv7 | AARCH64"
+  if [[ $(uname -m) == armv7* ]]; then arch=arm32; else arch=arm64; fi; echo "ARM Architecture: $arch"
+  url=https://raw.githubusercontent.com/devizer/glist/master/bin/megatools/megatools-$arch.tar.gz
+  cd /tmp; work=$(mktemp -d mega-XXXXXXXX)
+  pushd $work
+  wget --no-check-certificate -O _mega.tar.gz $url
+  tar xzf _mega.tar.gz
+  cd megatools*
+  sudo cp megatools /usr/local/bin/megatools
+  popd
+  rm -rf $work
+fi
+megatools --version | grep 'command line tools'
+
+
 
