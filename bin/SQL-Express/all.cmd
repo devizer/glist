57a17862ac50d6ddc3f637f2388685154a380ea1
devizer <devizer@users.github.com>
2019-02-07 07:04:51 +0200

    Added firewall rules for SQL Servers
    Former-commit-id: e9a2176e5a6fafc718703be75e4ee50df5103acd

--- bin/SQL-Express/all.cmd
+++ bin/SQL-Express/all.cmd
@@ -1,13 +1,35 @@
+@echo off
+netsh advfirewall firewall add rule name="All ICMP V4" protocol=icmpv4:any,any dir=in action=allow
+netsh advfirewall firewall add rule name="Open Port 1433 (SQL Server)" dir=in action=allow protocol=TCP localport=1433
+
+title [1/10] SQL Server 2017 Developer Edition
 type sql-dev-2017.ps1 | powershell -c -
+
+title [2/10] SQL Server 2017 Express
 call SQL-Express-2017-Updated.cmd 
+
+title [3/10] SQL Server 2016 Express
 call SQL-Express-2016-Updated.cmd
+
+title [4/10] SQL Server 2014 x86 Express
 call SQL-Express-2014-SP2-x86.cmd
+
+title [5/10] SQL Server 2014 Express
 call SQL-Express-2014-SP1.cmd
+
+title [6/10] SQL Server 2012 Express
 call SQL-Express-2012-SP3.cmd
+
+title [7/10] SQL Server 2008 R2 x86 Express
 call SQL-Express-2008-R2-SP2-x86.cmd
+
+title [8/10] SQL Server 2008 Express
 call SQL-Express-2008-SP3-x64.cmd 
+
+title [9/10] SQL Server 2005 Express x86
 call SQL-Express-2005-SP4-x86.cmd
 
+title [10/10] SQL Server Managment Studio 2016/2017
 call SSMS-Setup.cmd
 
 type list-sql-services.ps1 | powershell -c -
---
+++ windows-system-info/detect-environment.ps1
@@ -0,0 +1,50 @@
+function GetCrossPlatformInfo()
+{
+  # true if classic powershell.exe, but not a Nano Server
+$info = @{
+  IsDesktop = -not ($PSVersionTable.PSEdition -eq "Core");
+  IsWindows = ("Win32NT" -eq [Environment]::OSVersion.Platform);
+  IsNanoServer = ((get-command "Get-ComputerInfo" -errorAction SilentlyContinue) -and ("Nano Server" -eq ((get-computerinfo -Property WindowsInstallationType).WindowsInstallationType)));
+}
+# Platform is one of: Windows | Linux | Darwin | FreeBSD | Unix
+if ($info.IsWindows) { $info.Platform = "Windows" } else { $info.Platform = "Unix (unknown)"; try { $info.Platform = ((uname -s) | out-string 2>$null).Trim(@([char] 13, [char] 10, [char]32)) } catch {} }
+$info
+
+
+  @{IsDesktop=$isDesktop; IsWindows=$isWindows; IsNanoServer=$isNanoServer };
+
+  if ((get-command "Get-ComputerInfo" -errorAction SilentlyContinue) -and ("Nano Server" -eq ((get-computerinfo -Property WindowsInstallationType).WindowsInstallationType))) {
+    if ("Nano Server" -eq ((get-computerinfo -Property WindowsInstallationType).WindowsInstallationType) { 
+      $ret.Kind = "Nano"; 
+      $ret.Description = "Nano Server"
+    }
+  }
+
+  $ret = @{Kind="Desktop"; IsCore=$false; Description="Classic powershell on Windows XP ... Server 2019 including Server Core)"};
+  if ($PSVersionTable.PSEdition -eq "Core") { 
+    $ret = @{Kind=$null; IsCore=$true; Description=$null};
+    # Either pwsh or Nano Server.
+    # First, check nano server
+    if (get-command "Get-ComputerInfo" -errorAction SilentlyContinue) {
+      if ("Nano Server" -eq ((get-computerinfo -Property WindowsInstallationType).WindowsInstallationType)) { 
+        $ret.Kind = "Nano"; 
+        $ret.Description = "Nano Server"
+      }
+    }
+    if ($ret.Kind -eq $null) { 
+      # pwsh on either Windows or Unix
+      if ("Win32NT" -eq [Environment]::OSVersion.Platform) {
+        $ret.Kind="Windows-Core";
+        $ret.Description="pwsh core on Windows, but not a Nano Server"
+      }
+      else
+      {
+        $ret.Kind="Unix-Core";
+        $ret.Description="pwsh core on Unix (either linux or MacOS)"
+      }
+    }
+  }
+  return $ret;
+}
+$environment
+if ($PSVersionTable.PSEdition -eq "Core") { Write-Host "Core"; }
