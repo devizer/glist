63ba16031fb8c2a6512b493aa21553e7b994c0b1
devizer <devizer@users.github.com>
2019-03-03 04:55:39 +0200

    Adding support for ARMv7 | AARCH64 | X64 megatools
    Former-commit-id: cce8ad6f7072d7e53dc8019b591c97e29bf42656

--- bin/megatools/install.sh
+++ bin/megatools/install.sh
@@ -1,10 +1,11 @@
 #!/usr/bin/env bash
+# Origin: https://megatools.megous.com/builds/experimental/
 # Here is one line installer 
 # wget -q -nv --no-check-certificate -O - https://raw.githubusercontent.com/devizer/glist/master/bin/megatools/install.sh | bash
 
 if [ "$(command -v megatools)" == "" ]; then
-  echo "Installing megatools for ARMv7 | AARCH64"
-  if [[ $(uname -m) == armv7* ]]; then arch=arm32; else arch=arm64; fi; echo "ARM Architecture: $arch"
+  echo "Installing megatools for ARMv7 | AARCH64 | X64 Linux"
+  if [[ $(uname -m) == armv7* ]]; then arch=arm32; else arch=arm64; fi; if [[ $(uname -m) == x86_64 ]]; then arch=x64; fi; echo "Architecture: $arch"
   url=https://raw.githubusercontent.com/devizer/glist/master/bin/megatools/megatools-$arch.tar.gz
   cd /tmp; work=$(mktemp -d mega-XXXXXXXX)
   pushd $work
