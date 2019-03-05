d4f36a16579dad637d858af4c3e32ce64ab5a47e
devizer <devizer@users.github.com>
2019-03-05 09:19:52 +0200

    Updating

--- windows-system-info/detect-environment.ps1
+++ windows-system-info/detect-environment.ps1
@@ -4,14 +4,13 @@
 # [bool] IsWindows
 function Get-CrossPlatformInfo()
 {
-  $installationType = (Get-ItemProperty -Path 'HKLM:\SOFTWARE\Microsoft\Windows NT\CurrentVersion').InstallationType
-
   $info = @{
     IsCore = ($PSVersionTable.PSEdition -eq "Core");
     IsWindows = ("Win32NT" -eq [Environment]::OSVersion.Platform);
     IsNanoServer = $false;
     Platform = "Unknown";
   }
+  
   # Platform is one of: Windows | Linux | Darwin | FreeBSD | Unix (unknown)
   if ($info.IsWindows) { 
     $info.Platform = "Windows";
