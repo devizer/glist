305bc8007490e0395a19ad438523d52c83cb3f9e
devizer <devizer@users.github.com>
2019-03-15 12:11:08 +0200

    Updating versions of SQL LocalDB

--- bin/sql-LocalDB/Install-SQL-LocalDB.ps1
+++ bin/sql-LocalDB/Install-SQL-LocalDB.ps1
@@ -1,43 +1,62 @@
 # One Line Installer
 # @"%SystemRoot%\System32\WindowsPowerShell\v1.0\powershell.exe" -NoProfile -InputFormat None -ExecutionPolicy Bypass -Command "[System.Net.ServicePointManager]::ServerCertificateValidationCallback={$true}; iex ((New-Object System.Net.WebClient).DownloadString('https://raw.githubusercontent.com/devizer/glist/master/bin/sql-LocalDB/Install-SQL-LocalDB.ps1'))"
-$files=$("Essentials.7z.exe")
-$baseUrl="https://raw.githubusercontent.com/devizer/glist/master/Essentials/"
-$Temp="$($Env:LocalAppData)"; if ($Temp -eq "") { $Temp="$($Env:UserProfile)"; }
-$Temp="$Temp\Temp"
-$Essentials="$Temp\Essentials"
-Write-Host "Essentials folder: $Essentials"
-New-Item $Essentials -type directory -force -EA SilentlyContinue | out-null
-[System.Net.ServicePointManager]::ServerCertificateValidationCallback={$true};
-foreach($file in $files) {
- $d=new-object System.Net.WebClient; $d.DownloadFile("$baseurl/$file","$Essentials\$file");
+function Download-Essentials {
+  $files=$("Essentials.7z.exe")
+  $baseUrl="https://raw.githubusercontent.com/devizer/glist/master/Essentials/"
+  $Temp="$($Env:LocalAppData)"; if ($Temp -eq "") { $Temp="$($Env:UserProfile)"; }
+  $Temp="$Temp\Temp"
+  $Essentials="$Temp\Essentials"
+  Write-Host "downloading essentials (7z, parallel-download) to the [$Essentials] folder"
+  New-Item $Essentials -type directory -force -EA SilentlyContinue | out-null
+  [System.Net.ServicePointManager]::ServerCertificateValidationCallback={$true};
+  foreach($file in $files) {
+    $d=new-object System.Net.WebClient; $d.DownloadFile("$baseurl/$file","$Essentials\$file");
+  }
+  pushd $Essentials
+  $extract_log = (& .\Essentials.7z.exe -y 2>&1)
+  ri Essentials.7z.exe -force -EA SilentlyContinue | out-null
+  popd
+  $isX64 = ("$($Env:PROCESSOR_ARCHITECTURE)" -eq "AMD64");
+  If ($isX64) { $_7_Zip = "$Essentials\x64\7z.exe"; } Else { $_7_Zip="$Essentials\x86\7z.exe"; }
+  Write-Host "Architecture: $($Env:PROCESSOR_ARCHITECTURE). 7-Zip: $_7_Zip";
+  return @{
+    IsX64 = $isX64;
+    Temp = $Temp;
+    Essentials = $Essentials;
+    SevenZip = $_7_Zip;
+    ParallelDownloader = "$Essentials\Parallel-Download.exe";
+    Tee = "$Essentials\Tee.exe"
+  }
 }
-pushd $Essentials
-& .\Essentials.7z.exe -y
-ri Essentials.7z.exe
-popd
-$_7_Zip="$Essentials\x64\7z.exe";  if ( -Not ("$($Env:PROCESSOR_ARCHITECTURE)" -eq "AMD64")) { $_7_Zip="$Essentials\x86\7z.exe"; }
-Write-Host "Architecture: $($Env:PROCESSOR_ARCHITECTURE). 7-Zip: $_7_Zip";
 
-# $suffix="v13-x64"; if ( -Not ("$($Env:PROCESSOR_ARCHITECTURE)" -eq "AMD64")) { $suffix="v11-x86"; }
-$suffix="v14-x64"; if ( -Not ("$($Env:PROCESSOR_ARCHITECTURE)" -eq "AMD64")) { $suffix="v12-x86"; }
-$pars=@("`"$Temp\LocalDB-Installer`"", "https://raw.githubusercontent.com/devizer/glist/master/bin/sql-LocalDB/Sql-LocalDB-$suffix.msi")
-pushd $Temp
-& "$Essentials\Parallel-Download.exe" $pars
+$Essentials = Download-Essentials;
+
+$download_To="$($Essentials.Temp)\LocalDB-Installer"
+if ($essentials.IsX64) { $suffix="v14-x64"; } Else { $suffix="v12-x86"; }
+$pars=@("`"$download_To`"", "https://raw.githubusercontent.com/devizer/glist/master/bin/sql-LocalDB/Sql-LocalDB-$suffix.msi")
+pushd "$($Essentials.Temp)"
+& "$($Essentials.ParallelDownloader)" $pars
 popd
 
-Write-Host "Installing .NET 3.5 and 4.5"
-Add-WindowsFeature Net-Framework-Core -EA SilentlyContinue
-# Add-WindowsFeature NET-Framework-45-Core -EA SilentlyContinue
+if (Get-Command Add-WindowsFeature -errorAction SilentlyContinue)
+{
+    Write-Host "Installing .NET 3.5 and 4.5"
+    Add-WindowsFeature Net-Framework-Core -EA SilentlyContinue
+    Add-WindowsFeature NET-Framework-45-Core -EA SilentlyContinue
+}
+
+Write-Host "Suspending .NET NGEN Queue"
 pushd "$Env:windir\microsoft.net"
-& Framework64\v2.0.50727\ngen.exe  queue pause
-& Framework64\v4.0.30319\ngen.exe  queue pause
-& Framework\v2.0.50727\ngen.exe    queue pause
-& Framework\v4.0.30319\ngen.exe    queue pause
+$__ = (& Framework64\v2.0.50727\ngen.exe  queue pause 2>&1)
+$__ = (& Framework64\v4.0.30319\ngen.exe  queue pause 2>&1)
+$__ = (& Framework\v2.0.50727\ngen.exe    queue pause 2>&1)
+$__ = (& Framework\v4.0.30319\ngen.exe    queue pause 2>&1)
 popd
 
-pushd "$temp\LocalDB-Installer"
+pushd "$download_To"
 Write-Host "Installing SQL-LocalDB-$suffix.MSI ..."
 cmd /c msiexec /i "SQL-LocalDB-$suffix.MSI" IACCEPTSQLLOCALDBLICENSETERMS=YES /qn /L*v SqlLocaLDB-$suffix.log
+Write-Host "Log file is $download_To/SqlLocaLDB-$suffix.log"
 popd
 
-Remove-Item -Force "$temp\LocalDB-Installer\*.MSI"
+Remove-Item -Force "$download_To\*.MSI"
