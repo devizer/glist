a4758aaf067f9aa1aa4d5d5cde85940c4b620fca
devizer <devizer@users.github.com>
2019-06-02 02:30:14 +0300

    SSMS 2016 & 2017

--- bin/SQL-Express/SSMS-Setup.cmd
+++ bin/SQL-Express/SSMS-Setup.cmd
@@ -33,6 +33,7 @@ If Defined IS_WINDOWS_6_1_OR_ABOVE (If Defined IS_X64_WINDOWS (
   set url=https://download.microsoft.com/download/9/3/3/933EA6DD-58C5-4B78-8BEC-2DF389C72BE0/SSMS-Setup-ENU.exe
   set v=2016
   set params=%Params_New%
+  set Need_Extract=
 ))
 
 If Defined IS_WINDOWS_6_2_OR_ABOVE (If Defined IS_X64_WINDOWS (
@@ -40,6 +41,7 @@ If Defined IS_WINDOWS_6_2_OR_ABOVE (If Defined IS_X64_WINDOWS (
   set url=https://download.microsoft.com/download/D/D/4/DD495084-ADA7-4827-ADD3-FC566EC05B90/SSMS-Setup-ENU.exe
   set v=2017
   set params=%Params_New%
+  set Need_Extract=
 ))
 
 :TEST
