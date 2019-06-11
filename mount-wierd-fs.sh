b8788da9534fb2250507ba62241edc22cfd314fa
devizer <devizer@users.github.com>
2019-06-11 09:01:28 +0300

    mount-wierd-fs: added DIRECT-IO=on

--- mount-wierd-fs.sh
+++ mount-wierd-fs.sh
@@ -7,4 +7,11 @@ sudo dd if=/dev/zero of="/$file" bs=1 seek=3G count=1
 sudo mkfs.btrfs -L a-disk "$file"
 sudo mkdir -p "$path"
 # sudo mount -t btrfs "$file" "$path" -o defaults,noatime,nodiratime,ssd,compress-force=zlib
-sudo mount -t btrfs "$file" "$path" -o defaults,noatime,nodiratime,compress-force=zlib
+
+function mount_without_Direct_IO() {
+  sudo mount -t btrfs "$file" "$path" -o defaults,noatime,nodiratime,compress-force=zlib
+}
+
+
+losetup /dev/loop0 "$file" --direct-io=on
+sudo mount -t btrfs /dev/loop0 "$path" -o defaults,noatime,nodiratime,compress-force=zlib
