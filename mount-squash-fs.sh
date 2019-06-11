695f500b9c25db0d21a7735cca5cc21b78c72d27
devizer <devizer@users.github.com>
2019-06-11 15:20:32 +0300

    mount-squash-fs

--- mount-squash-fs.sh
+++ mount-squash-fs.sh
@@ -4,7 +4,7 @@
 curl -kL -o kubuntu-18.10-desktop-amd64.iso http://cdimage.ubuntu.com/kubuntu/releases/18.10/release/kubuntu-18.10-desktop-amd64.iso
 mkdir -p /mnt/dvd "/live linux"
 mount -o loop kubuntu-18.10-desktop-amd64.iso /mnt/dvd
-mount -o loop /mnt/dvd/casper/filesystem.squashfs /kubuntu-dvd "/live linux"
+mount -o loop /mnt/dvd/casper/filesystem.squashfs "/live linux"
 
 
 
