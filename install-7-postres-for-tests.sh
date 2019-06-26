4a49bd6a7eb4ec38f35955359d72fff92243a899
devizer <devizer@users.github.com>
2019-06-26 19:48:33 +0300

    install-7-postres-for-tests

---
+++ install-7-postres-for-tests.sh
@@ -0,0 +1,45 @@
+#!/us/bin/env bash
+# url=https://raw.githubusercontent.com/devizer/glist/master/install-7-postres-for-tests.sh; (wget -q -nv --no-check-certificate -O - $url 2>/dev/null || curl -sSL $url) | bash
+set -e
+set -u
+
+POSTGRESQL_DB="${POSTGRESQL_DB:-APP42}"
+POSTGRESQL_PASS="${POSTGRESQL_PASS:-pass}"
+POSTGRESQL_USER="${POSTGRESQL_USER:-postgres}"
+WAIT_TIMEOUT="${WAIT_TIMEOUT:-30}"
+
+function wait_for() {
+  if [[ "$(command -v psql)" == "" ]]; then return; fi
+  p=$1
+  printf "Waiting for postgres on localhost at $p port ...."
+  counter=0; total=$WAIT_TIMEOUT; started=""
+  while [ $counter -lt $total ]; do
+    counter=$((counter+1));
+    PGCONNECT_TIMEOUT=1 PGPASSWORD="$POSTGRESQL_PASS" psql -t -h localhost -p "$p" -U "$POSTGRESQL_USER" -q -c "select 1;" >/dev/null 2>&1 && started="yes" || true
+    if [ -n "$started" ]; then printf " OK\n"; break; else (sleep 1; printf $counter"."); fi
+  done
+  if [ -z "$started" ]; then printf " Fail\n"; fi;
+}
+
+vars="-e POSTGRESQL_USER=$POSTGRESQL_USER -e POSTGRESQL_PASS=$POSTGRESQL_PASS -e POSTGRESQL_DB=$$POSTGRESQL_DB -e POSTGRES_USER=$POSTGRESQL_USER -e POSTGRES_PASS=$POSTGRESQL_PASS -e POSTGRES_DB=$POSTGRESQL_DB"
+eval "docker run --name postgres-12  $vars -p 54328:5432 -d postgres:12"
+eval "docker run --name postgres-11  $vars -p 54327:5432 -d postgres:11.4"
+eval "docker run --name postgres-10  $vars -p 54326:5432 -d postgres:10.9"
+eval "docker run --name postgres-9.6 $vars -p 54325:5432 -d postgres:9.6"
+eval "docker run --name postgres-9.5 $vars -p 54324:5432 -d postgres:9.5"
+eval "docker run --name postgres-9.4 $vars -p 54323:5432 -d postgres:9.4"
+eval "docker run --name postgres-9.1 $vars -p 54322:5432 -d postgres:9.1"
+eval "docker run --name postgres-8.4 $vars -p 54321:5432 -d postgres:8.4"
+
+for port in {54328..54321}; do
+  wait_for $port
+done
+
+for port in {54321..54328}; do
+  printf "checking port $port ...";
+  cmd1='PGPASSWORD=pass psql -t -h localhost -p '$port' -U postgres -q -c "select version();"';
+  v1="unknown"; v1=$(eval $cmd1); v1="${v1## }";
+  cmd2='PGPASSWORD=pass psql -t -h localhost -p '$port' -U postgres -q -c "show server_version;"';
+  v2="unknown"; v2=$(eval $cmd2); v2="${v2## }";
+  printf "\r$port: [$v2] $v1\n";
+done
