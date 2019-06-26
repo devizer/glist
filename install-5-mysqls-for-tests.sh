2e6461aa24882842449e4cf86e8268b2647787b2
devizer <devizer@users.github.com>
2019-06-26 18:37:06 +0300

    install-5-mysqls-for-tests: WAIT_TIMEMOUT with 30 secs as default

--- install-5-mysqls-for-tests.sh
+++ install-5-mysqls-for-tests.sh
@@ -5,13 +5,14 @@ set -u
 
 MYSQL_TEST_DB="${MYSQL_TEST_DB:-APP42}"
 MYSQL_ROOT_PASSWORD="${MYSQL_ROOT_PASSWORD:-D0tN3t}"
+WAIT_TIMEOUT="${WAIT_TIMEOUT:-30}"
 
 function wait_for() {
   if [[ "$(command -v mysql)" == "" ]]; then return; fi
   n=$1
   p=$2
   printf "Waiting for $n on localhost @ $p ...."
-  counter=0; total=30; started=""
+  counter=0; total=$WAIT_TIMEOUT; started=""
   while [ $counter -lt $total ]; do
     counter=$((counter+1));
     mysql --protocol=TCP -h localhost -u root -p"${MYSQL_ROOT_PASSWORD}" -P $p -e "Select 1;" 2>/dev/null 1>&2 && started="yes" || true
@@ -26,12 +27,20 @@ echo "Installing $count mysql servers: ${names[*]// /|}"
 for (( i=0; i<$count; i++ )); do
   image=${images[$i]}
   name=${names[$i]}
-  echo "[$(($i+1)) / $count] container: $name, image: $image"
+  echo "[$(($i+1)) / $count] starting container: $name, image: $image"
   time sudo docker pull "$image"
   port=$((3306+1+$i));
   cmd="sudo docker run --name $name -e MYSQL_ROOT_HOST=% -e MYSQL_ROOT_PASSWORD=\"${MYSQL_ROOT_PASSWORD}\" -e MYSQL_DATABASE=\"${MYSQL_TEST_DB}\" -d -p $port:3306 $image"
   echo ""; echo $cmd
   eval "$cmd" || true
+done
+
+echo "Checking $count mysql servers: ${names[*]// /|}"
+for (( i=0; i<$count; i++ )); do
+  image=${images[$i]}
+  name=${names[$i]}
+  port=$((3306+1+$i));
+  echo "[$(($i+1)) / $count] wainting for: $name, image: $image, port: $port"
   wait_for "$name" "$port"
   # echo "LOGS of $name"; sudo docker logs "$name"
   if [[ "$(command -v mysql)" != "" ]]; then
