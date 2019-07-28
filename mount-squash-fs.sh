7849f4bb2eb0be2d5e90d7efa9bdb62c66f5b578
devizer <devizer@users.github.com>
2019-07-28 07:59:34 +0300

    reuse mount-squash-fs.sh

--- mount-squash-fs.sh
+++ mount-squash-fs.sh
@@ -1,7 +1,10 @@
 #!/usr/bin/env bash
 
 # wget --no-check-certificate -O kubuntu-18.10-desktop-amd64.iso http://cdimage.ubuntu.com/kubuntu/releases/18.10/release/kubuntu-18.10-desktop-amd64.iso
-curl -kL -o kubuntu-18.10-desktop-amd64.iso http://cdimage.ubuntu.com/kubuntu/releases/18.10/release/kubuntu-18.10-desktop-amd64.iso
+if [ ! -f kubuntu-18.10-desktop-amd64.iso.ok ]; then
+  curl -kL -o kubuntu-18.10-desktop-amd64.iso http://cdimage.ubuntu.com/kubuntu/releases/18.10/release/kubuntu-18.10-desktop-amd64.iso \
+  && echo ok > kubuntu-18.10-desktop-amd64.iso.ok
+fi
 mkdir -p /mnt/dvd "/live linux"
 mount -o loop kubuntu-18.10-desktop-amd64.iso /mnt/dvd
 mount -o loop /mnt/dvd/casper/filesystem.squashfs "/live linux"
