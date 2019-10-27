c17cceec08c23d370a246d5c20839129611c0aa0
devizer <devizer@users.github.com>
2019-10-27 03:52:55 +0300

    makeup

--- bin/SQL-Express/sql-dev-2017.ps1
+++ bin/SQL-Express/sql-dev-2017.ps1
@@ -1,5 +1,6 @@
 $v=2017
 $KEY="SQL-Dev-$v-SP-x64"
+#     https://download.microsoft.com/download/5/A/7/5A7065A2-C81C-4A31-9972-8A31AC9388C1/SQLServer2017-SSEI-Dev.exe
 $url="https://download.microsoft.com/download/5/A/7/5A7065A2-C81C-4A31-9972-8A31AC9388C1/SQLServer2017-SSEI-Dev.exe"
 $outfile="${Env:AppData}\Temp\$KEY.exe"
 Write-Host "DOWNLOADING SQL $v BOOTSTRAPPER into `"$outfile`""
