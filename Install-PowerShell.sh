0faf2a772cf32291bf31bc489c1383cb1d6f8605
devizer <devizer@users.github.com>
2019-12-07 08:24:21 +0200

    adding framework depended Install-PowerShell.sh

--- Install-PowerShell.sh
+++ Install-PowerShell.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
-# INSTALL_DIR=/opt/powershell; VER="--stable|--prerelase"; url=https://raw.githubusercontent.com/devizer/glist/master/Install-PowerShell.sh; (wget -q -nv --no-check-certificate -O - $url 2>/dev/null || curl -ksSL $url) | bash -s $VER
+# export INSTALL_DIR=/opt/powershell; VER="stable|prerelase"; url=https://raw.githubusercontent.com/devizer/glist/master/Install-PowerShell.sh; (wget -q -nv --no-check-certificate -O - $url 2>/dev/null || curl -ksSL $url) | bash
 INSTALL_DIR=${INSTALL_DIR:-/opt/powershell}
 url=https://github.com/PowerShell/PowerShell/releases/download/v6.2.3/powershell-6.2.3-linux-x64-fxdependent.tar.gz
 if [[ "$1" == "--pre"* || "${VER:-}" == "pre"* ]]; then
@@ -16,7 +16,13 @@ function install_powershell() {
     tar xzf pwsh.tar.gz
     rm -f pwsh.tar.gz
     echo '#!/usr/bin/env bash
-    dotnet "'$INSTALL_DIR'/pwsh.dll" "$@"
+    if [[ -f "'$INSTALL_DIR'/pwsh.dll" ]]; then
+        dotnet "'$INSTALL_DIR'/pwsh.dll" "$@"
+        exit $?
+    else
+        echo "Missing framework depended Powershell: '$INSTALL_DIR'/pwsh.dll >&2
+        exit 1
+    fi
     ' | sudo tee /usr/local/bin/pwsh > /dev/null
     sudo chmod +x /usr/local/bin/pwsh
 
