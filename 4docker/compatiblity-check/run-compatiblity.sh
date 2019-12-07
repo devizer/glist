f11f5696cea7605efccdbfbed0330aaf59e7a4bb
devizer <devizer@users.github.com>
2019-12-07 07:49:14 +0200

    added framework depended Install-PowerShell.sh

--- 4docker/compatiblity-check/run-compatiblity.sh
+++ 4docker/compatiblity-check/run-compatiblity.sh
@@ -1,4 +1,6 @@
+# apt-get install docker-ce=5:18.09.0~3-0~ubuntu-bionic docker-ce-cli=5:18.09.0~3-0~ubuntu-bionic
 MULTIARCH_IMAGE="multiarch/debian-debootstrap:arm64-stretch"
+MULTIARCH_IMAGE="multiarch/debian-debootstrap:i386-stretch"
 DOTNET_VER="2.2"
 
 url=https://raw.githubusercontent.com/devizer/glist/master/4docker/compatiblity-check/dotnet-and-tests.sh
@@ -13,6 +15,7 @@ echo 'WORKS?
 2.2: multiarch/debian-debootstrap:aarch64-buster
 '
 
+docker exec -t tests bash -c 'echo "uname -m: $(uname -m)"'
 docker exec -t tests bash -c 'source /etc/os-release; echo Im $PRETTY_NAME; apt update; apt-get install -y -qq git sudo jq tar bzip2 gzip curl lsb-release procps gnupg apt-transport-https dirmngr ca-certificates mc htop nano'
 docker cp /tmp/dotnet-and-tests.sh tests:/dotnet-and-tests.sh
 docker exec -t tests bash -c "export DOTNET_VER=$DOTNET_VER; bash -eu /dotnet-and-tests.sh" || echo "FFFFFFFAAAAAAAAIIIIIIIIL"
