47aab2971107cef23063a59ddf19a86794197cb7
devizer <devizer@users.github.com>
2019-12-11 13:02:32 +0200

    added dotnet core sdk 2.2 for install-disk-benchmark-from-source

--- install-disk-benchmark-from-source.sh
+++ install-disk-benchmark-from-source.sh
@@ -2,8 +2,23 @@
 # script=https://raw.githubusercontent.com/devizer/glist/master/install-disk-benchmark-from-source.sh; (wget -q -nv --no-check-certificate -O - $script 2>/dev/null || curl -sSL $script) | bash
 
 function install_disk_benchmark_from_source() {
+
+   if [[ "$(command -v dotnet)" == "" ]]; then
+      export DOTNET_SYSTEM_GLOBALIZATION_INVARIANT=1
+      export DOTNET_SYSTEM_NET_HTTP_USESOCKETSHTTPHANDLER=0
+      url=https://raw.githubusercontent.com/devizer/glist/master/install-dotnet-dependencies.sh; (wget -q -nv --no-check-certificate -O - $url 2>/dev/null || curl -ksSL $url) | bash
+      DOTNET_Url=https://dot.net/v1/dotnet-install.sh; 
+      mkdir -p ~/tmp/dotnet ~/.dotnet/tools
+      export PATH="$PATH:$HOME/.dotnet/tools:$HOME/tmp/dotnet"
+      export DOTNET_ROOT="$HOME/tmp/dotnet"
+      curl -o /tmp/_dotnet-install.sh -ksSL $DOTNET_Url
+      
+      echo "Installing .NET Core 2.2 SDK"
+      bash /tmp/_dotnet-install.sh -c 2.2 -i ~/tmp/dotnet
+   fi
+   
    mkdir -p ~/build/disk-benchmark-source
-   pushd ~/build/disk-benchmark-source
+   pushd ~/build/disk-benchmark-source >/dev/null
      git clone https://github.com/devizer/KernelManagementLab
      cd KernelManagementLab/BenchmarkLab
      git pull
@@ -13,11 +28,12 @@ function install_disk_benchmark_from_source() {
      exe="$(pwd)/Universe.Benchmark.dll"
      echo '#!/usr/bin/env bash
         set -e
+        export DOTNET_SYSTEM_GLOBALIZATION_INVARIANT=1
         export DOTNET_ROOT="'$(dirname $dotnet)'"
         '$dotnet' '$exe' "$@"
      ' | sudo tee /usr/local/bin/disk-benchmark
      sudo chmod +x /usr/local/bin/disk-benchmark
-   popd
+   popd >/dev/null
    disk-benchmark --help
 }
 
