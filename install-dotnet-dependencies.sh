b760bc438f5e376bc48864dde12967d72f900ac5
devizer <devizer@users.github.io>
2020-05-06 14:59:17 +0300

    install-dotnet-dependencies.sh: comments

--- install-dotnet-dependencies.sh
+++ install-dotnet-dependencies.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 # Here is one line installer 
-# url=https://raw.githubusercontent.com/devizer/glist/master/install-dotnet-dependencies.sh; (wget -q -nv --no-check-certificate -O - $url 2>/dev/null || curl -ksSL $url) | UPDATE_REPOS=true bash -e
+# url=https://raw.githubusercontent.com/devizer/glist/master/install-dotnet-dependencies.sh; (wget -q -nv --no-check-certificate -O - $url 2>/dev/null || curl -ksSL $url) | UPDATE_REPOS=true bash -e && echo "Successfully installed .NET Core Dependencies"
 
 function smart_sudo() {
   if [[ -n "$(command -v sudo || true)" ]]; then
