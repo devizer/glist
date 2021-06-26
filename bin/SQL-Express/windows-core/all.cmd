02701868d32be9ba34217fd1a87f830fb86d4018
devizer <devizer@users.github.io>
2021-06-26 16:16:40 +0300

    minor fix

--- bin/SQL-Express/windows-core/all.cmd
+++ bin/SQL-Express/windows-core/all.cmd
@@ -1,6 +1,9 @@
 set archive=sql-express-all.7z.exe
 set url=https://raw.githubusercontent.com/devizer/glist/master/bin/SQL-Express/windows-core/%archive%
 set outdir=%AppData%\Temp\SQL-Express-All
+If Defined LOCALAPPDATA (
+  set outdir=%LOCALAPPDATA%\Temp\SQL-Express-All
+)
 set outfile=%outdir%\%archive%
 mkdir "%outdir%" 1>nul 2>&1
 echo [System.Net.ServicePointManager]::ServerCertificateValidationCallback={$true}; $d=new-object System.Net.WebClient; $d.DownloadFile("$Env:url","$Env:outfile") | powershell -command -
