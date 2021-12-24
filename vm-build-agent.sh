b7bbf77731d2e8f7f35c4e1810b661ebff7bac4e
devizer <devizer@github.users>
2021-12-24 06:56:50 +0200

    vm-build-agent: download progress

--- vm-build-agent.sh
+++ vm-build-agent.sh
@@ -31,7 +31,7 @@ function DownloadVM() {
       Say "
 FROM: $url
   TO: $full_name"
-      wget --no-check-certificate -O "$full_name" $url || curl -kSL -o "$full_name" $url
+      wget -q --show-progress --progress=bar:force --no-check-certificate -O "$full_name" $url || curl -ksSL -o "$full_name" $url
       if [ $? -ne 0 ]; then
         rm -f "$full_name"
       else
