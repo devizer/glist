6e12f3b626c57d4583c05397bcbbfdaf7beb70b0
devizer <devizer@github.users>
2022-02-10 01:05:04 +0200

    Install-Power-Shell: added aria2

--- Install-Latest-PowerShell.sh
+++ Install-Latest-PowerShell.sh
@@ -2,6 +2,36 @@
 # export PSVER=7.2.1 PSDIR=/opt/pwsh
 # url=https://raw.githubusercontent.com/devizer/glist/master/Install-Latest-PowerShell.sh; (wget -q -nv --no-check-certificate -O - $url 2>/dev/null || curl -ksSL $url) | bash
 
+function download_file() {
+  local url="$1"
+  local file="$2";
+  local progress1="" progress2="" progress3="" 
+  if [[ "$DOWNLOAD_SHOW_PROGRESS" != "True" ]] || [[ ! -t 1 ]]; then
+    progress1="-q -nv"       # wget
+    progress2="-s"           # curl
+    progress3="--quiet=true" # aria2c
+  fi
+  local try1=""
+  if [[ "$(command -v aria2c)" != "" ]]; then
+    [[ -n "${try1:-}" ]] && try1="$try1 || "
+    try1="aria2c $progress3 --allow-overwrite=true --check-certificate=false -s 9 -x 9 -k 4M -j 9 -d '$(dirname "$file")' -o '$(basename "$file")' '$url'"
+  fi
+  if [[ "$(command -v curl)" != "" ]]; then
+    [[ -n "${try1:-}" ]] && try1="$try1 || "
+    try1="${try1:-} curl $progress2 -f -kSL -o '$file' '$url'"
+  fi
+  if [[ "$(command -v wget)" != "" ]]; then
+    [[ -n "${try1:-}" ]] && try1="$try1 || "
+    try1="${try1:-} wget $progress1 --no-check-certificate -O '$file' '$url'"
+  fi
+  if [[ "${try1:-}" == "" ]]; then
+    echo "error: niether curl, wget or aria2c is available"
+    exit 42;
+  fi
+  eval $try1 || eval $try1 || eval $try1
+  # eval try-and-retry wget $progress1 --no-check-certificate -O '$file' '$url' || eval try-and-retry curl $progress2 -kSL -o '$file' '$url'
+}
+
 function Install_PowerShell() {
   local tmp="${TMPDIR:-/tmp}"
   PSDIR="${PSDIR:-/opt/pwsh}"
@@ -27,7 +57,9 @@ function Install_PowerShell() {
   Say  "Downloading PowerShell [$PSVER] for [$(uname -m)] into [$PSDIR]"
   echo "       url: $url"
   echo "      file: $tmp/$file"
-  try-and-retry curl -kSL -o "$tmp/$file" "$url" || try-and-retry wget --no-check-certificate -O "$tmp/$file" "$url" || rm -f "$tmp/$file"
+  # try-and-retry curl -kSL -o "$tmp/$file" "$url" || try-and-retry wget --no-check-certificate -O "$tmp/$file" "$url" || rm -f "$tmp/$file"
+  DOWNLOAD_SHOW_PROGRESS=True
+  download_file "$url" "$tmp/$file"
   sudo mkdir -p "$PSDIR"
   pushd $PSDIR >/dev/null
   if [[ -n "$(command -v pv)" ]]; then pv "$tmp/$file" | sudo tar xzf -; else sudo tar xzf "$tmp/$file"; fi
