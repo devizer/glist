c6333616e107bdc5b4b973402d8a62b515bc7474
devizer <devizer@github.users>
2022-03-25 07:02:00 +0200

    12th download-libMono-Unix-so.sh

--- bin/libMono.Unix.so/download-libMono-Unix-so.sh
+++ bin/libMono.Unix.so/download-libMono-Unix-so.sh
@@ -10,21 +10,21 @@ machine=$(uname -m);
 [[ "$(dpkg --print-architecture)" == armel ]] && machine=arm32v5
 
 url="https://raw.githubusercontent.com/devizer/glist/master/bin/libMono.Unix.so/${machine}/libMono.Unix.so"
-file=/tmp/libMono.Unix.so
+sofile=/tmp/libMono.Unix.so
 echo "Downloading libMono.Unix.so for the current cpu arch
   url: $url
- file: $file"
+ file: $sofile"
 
-curl -fkSL -o "$file" "$url" || curl -fkSL -o "$file" "$url" || curl -fkSL -o "$file" "$url"
-echo "Downloaded $file"
-ls -la $file
+curl -fkSL -o "$sofile" "$url" || curl -fkSL -o "$sofile" "$url" || curl -fkSL -o "$sofile" "$url"
+echo "Downloaded $sofile:"
+ls -la $sofile
 
 dir="${1:-$(pwd)}"
 echo "Searching Mono.Unix.dll in [$dir]"
-find "$dir" -name "Mono.Unix.dll" | while read file; do
-  to="$(dirname "$file")"
+find "$dir" -name "Mono.Unix.dll" | while read dllfile; do
+  to="$(dirname "$dllfile")"
   echo "Copying libMono.Unix.so to [$to]"
-  cp -fv "$file" "$to/libMono.Unix.so"
+  cp -fv "$sofile" "$to/libMono.Unix.so"
 done
 echo "Done. Below is results"
-ls $(find "$dir" -name "*Mono.Unix*") # without quotes
+ls -la $(find "$dir" -name "*Mono.Unix*") # without quotes
