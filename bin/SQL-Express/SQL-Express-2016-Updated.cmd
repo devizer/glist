3ebe9b021f234f7b7f5a4b89b566b45adab34818
devizer <devizer@users.github.io>
2023-02-23 19:24:35 +0200

    SQL 2016 retry on download

--- bin/SQL-Express/SQL-Express-2016-Updated.cmd
+++ bin/SQL-Express/SQL-Express-2016-Updated.cmd
@@ -12,7 +12,8 @@ echo Installing new instance [%NEW_SQL_INSTANCE_NAME%] of [%KEY%]
 set url=https://download.microsoft.com/download/9/0/7/907AD35F-9F9C-43A5-9789-52470555DB90/ENU/SQLEXPR_x64_ENU.exe
 set outfile=%AppData%\Temp\%KEY%.exe
 mkdir "%AppData%\Temp" 1>nul 2>&1
-echo [System.Net.ServicePointManager]::ServerCertificateValidationCallback={$true}; $d=new-object System.Net.WebClient; $d.DownloadFile("$Env:url","$Env:outfile") | powershell -command -
+rem echo [System.Net.ServicePointManager]::ServerCertificateValidationCallback={$true}; $d=new-object System.Net.WebClient; $d.DownloadFile("$Env:url","$Env:outfile") | powershell -command -
+echo [System.Net.ServicePointManager]::ServerCertificateValidationCallback={$true}; for ($i=1; $i -le 3; $i++) { $d=new-object System.Net.WebClient; try { $d.DownloadFile("$Env:url","$Env:outfile"); exit 0; } catch { Write-Host $_; Write-Host "Try $i of 3 failed for $($Env:url)" -ForegroundColor DarkRed; } } Exit 1  | powershell -command -
 
 echo EXTRACTING SQL Express %v%
 "%outfile%" /qs /x:"%AppData%\Temp\%KEY%\extracted"
