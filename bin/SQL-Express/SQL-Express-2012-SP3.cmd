8a3fa1cb8b8efe78fa8302893d3581bb1ed3c67c
devizer <devizer@users.github.io>
2023-03-03 10:51:42 +0200

    retry pattern for sql 2012

--- bin/SQL-Express/SQL-Express-2012-SP3.cmd
+++ bin/SQL-Express/SQL-Express-2012-SP3.cmd
@@ -11,7 +11,8 @@ echo DOWNLOADING %KEY%.exe
 set url=https://download.microsoft.com/download/F/6/7/F673709C-D371-4A64-8BF9-C1DD73F60990/ENU/x64/SQLEXPR_x64_ENU.exe
 set outfile=%AppData%\Temp\%KEY%.exe
 mkdir "%AppData%\Temp" 1>nul 2>&1
-echo [System.Net.ServicePointManager]::ServerCertificateValidationCallback={$true}; $d=new-object System.Net.WebClient; $d.DownloadFile("$Env:url","$Env:outfile") | powershell -command -
+rem echo [System.Net.ServicePointManager]::ServerCertificateValidationCallback={$true}; $d=new-object System.Net.WebClient; $d.DownloadFile("$Env:url","$Env:outfile") | powershell -command -
+echo [System.Net.ServicePointManager]::ServerCertificateValidationCallback={$true}; for ($i=1; $i -le 3; $i++) { $d=new-object System.Net.WebClient; try { $d.DownloadFile("$Env:url","$Env:outfile"); exit 0; } catch { Write-Host $_; Write-Host "Try $i of 3 failed for $($Env:url)" -ForegroundColor DarkRed; } } Exit 1  | powershell -command -
 
 echo Extracting %KEY%.exe
 "%outfile%" /q /x:"%AppData%\Temp\%KEY%"
