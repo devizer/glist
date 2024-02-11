746ab3162ef28882665d29a08bef0ae4b58dceae
devizer <devizer@users.github.io>
2024-02-11 11:11:16 +0200

    Fix SQL 2016 SP3 Express Setup

--- bin/SQL-Express/SQL-Express-2016-Updated.cmd
+++ bin/SQL-Express/SQL-Express-2016-Updated.cmd
@@ -1,27 +1,39 @@
 @echo off
 
-set v=2016
+set v=2016-Express
 echo DOWNLOADING SQL Express %v%
-set KEY=SQL-Express-%v%-SP-x64
+set KEY=SQL-%v%-SP-x64
 
 if Not Defined NEW_SQL_INSTANCE_NAME (
   set NEW_SQL_INSTANCE_NAME=SQL_2016
 )
 echo Installing new instance [%NEW_SQL_INSTANCE_NAME%] of [%KEY%]
 
+If Defined TEMP (
+  set Work=%TEMP%\%KEY%
+  Rem echo Creating Archive and Setup Folder "%TEMP%\%KEY%"
+  mkdir "%TEMP%\%KEY%" 1>nul 2>&1
+)
+If Not Exist "%Work%" Set "Work="
+If Not Defined Work (
+  If Defined LocalAppData (Set Work=%LocalAppData%\Temp\%KEY%) Else (Set Work=%AppData%\Temp\%KEY%)
+  mkdir "%Work%" 1>nul 2>&1
+)
+
+
 set url=https://download.microsoft.com/download/9/0/7/907AD35F-9F9C-43A5-9789-52470555DB90/ENU/SQLEXPR_x64_ENU.exe
 rem SP3
 set url=https://download.microsoft.com/download/f/9/8/f982347c-fee3-4b3e-a8dc-c95383aa3020/sql16_sp3_dlc/en-us/SQLEXPR_x64_ENU.exe
-set outfile=%AppData%\Temp\%KEY%.exe
-mkdir "%AppData%\Temp" 1>nul 2>&1
-rem echo [System.Net.ServicePointManager]::ServerCertificateValidationCallback={$true}; $d=new-object System.Net.WebClient; $d.DownloadFile("$Env:url","$Env:outfile") | powershell -command -
+set outfile=%Work%\%KEY%.exe
+echo %KEY% Archive and Setup Folder: [%Work%]
 echo [System.Net.ServicePointManager]::ServerCertificateValidationCallback={$true}; for ($i=1; $i -le 3; $i++) { $d=new-object System.Net.WebClient; try { $d.DownloadFile("$Env:url","$Env:outfile"); exit 0; } catch { Write-Host $_; Write-Host "Try $i of 3 failed for $($Env:url)" -ForegroundColor DarkRed; } } Exit 1  | powershell -command -
 
-echo EXTRACTING SQL Express %v%
-"%outfile%" /qs /x:"%AppData%\Temp\%KEY%\extracted"
+echo EXTRACTING SQL %v%
+"%outfile%" /qs /x:"%Work%\SETUPFILES\extracted"
+del /q "%outfile%" >nul 2>&1
 
 echo Starting Setup.exe
-"%AppData%\Temp\%KEY%\extracted\Setup.exe" /QUIETSIMPLE /ENU /INDICATEPROGRESS /ACTION=Install ^
+"%Work%\SETUPFILES\extracted\Setup.exe" /QUIETSIMPLE /ENU /INDICATEPROGRESS /ACTION=Install ^
   /IAcceptSQLServerLicenseTerms /IACCEPTROPENLICENSETERMS ^
   /UpdateEnabled=True ^
   /FEATURES=SQLENGINE,REPLICATION,SQL ^
@@ -38,5 +50,4 @@ echo Starting Setup.exe
   /SQLSYSADMINACCOUNTS="BUILTIN\ADMINISTRATORS" ^
   /TCPENABLED=1 /NPENABLED=1
 
-rd /q /s "%AppData%\Temp\%KEY%"
-del /Q /F "%outfile%"
+rd /q /s "%Work%"
