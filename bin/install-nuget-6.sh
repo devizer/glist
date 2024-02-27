efcb919533001179752a647bd8cbb5ba1ef99d04
devizer <devizer@users.github.io>
2024-02-27 18:49:27 +0200

    nuget 6

---
+++ bin/install-nuget-6.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+# url=https://raw.githubusercontent.com/devizer/glist/master/bin/install-nuget-6.sh; (wget -q -nv --no-check-certificate -O - $url 2>/dev/null || curl -ksSL $url) | bash
+
+f=/usr/local/share/nuget/nuget-latest.exe
+url=https://dist.nuget.org/win-x86-commandline/v3.4.4/nuget.exe
+url=https://dist.nuget.org/win-x86-commandline/latest/nuget.exe
+
+sudo mkdir -p "$(dirname "$f")"
+echo "Downloading '$url' as '$f'"
+sudo curl -ksfSL -o "$f" "$url" || sudo curl -ksfSL -o "$f" "$url" || sudo curl -ksfSL -o "$f" "$url" || sudo rm -f "$f"
+
+echo '#!/bin/sh
+mono '$f' "$@"
+' | sudo tee /usr/local/bin/nuget
+sudo chmod +x /usr/local/bin/nuget 
+
