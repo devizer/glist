6b85665f9f2f7ca234958f26ad5a87171efd97d3
devizer <devizer@users.github.io>
2024-03-20 06:08:17 +0200

    net test runners on termux

--- bin/net-test-runners.sh
+++ bin/net-test-runners.sh
@@ -1,5 +1,36 @@
 #!/usr/bin/env bash
 # url=https://raw.githubusercontent.com/devizer/glist/master/bin/net-test-runners.sh; (wget -q -nv --no-check-certificate -O - $url 2>/dev/null || curl -sSL $url) | bash
+
+if [[ -z "${NET_TEST_RUNNERS_INSTALL_DIR:-}" ]]; then
+  defult_target_dir=/opt
+  if [[ -n "${TERMUX_VERSION}" ]] && [[ -n "${PREFIX}" ]] && [[ -d "${PREFIX}" ]]; then
+    defult_target_dir="$PREFIX/share/net-test-runners"
+  fi
+
+  if [[ "$(uname -s)" == *"MINGW"* ]] && [[ -d "C:\\Windows" ]]; then
+    defult_target_dir="C:\\Windows"
+  fi
+
+  NET_TEST_RUNNERS_INSTALL_DIR="$defult_target_dir"
+fi
+
+if [[ -z "${LINKS_FOLDER:-}" ]]; then
+  defult_target_dir=/usr/local/bin
+  if [[ -n "${TERMUX_VERSION}" ]] && [[ -n "${PREFIX}" ]] && [[ -d "${PREFIX}" ]]; then
+    defult_target_dir="$PREFIX/bin"
+  fi
+
+  if [[ "$(uname -s)" == *"MINGW"* ]] && [[ -d "C:\\Windows" ]]; then
+    defult_target_dir="C:\\Windows"
+  fi
+
+  LINKS_FOLDER="$defult_target_dir"
+fi
+
+export NET_TEST_RUNNERS_INSTALL_DIR
+export LINKS_FOLDER
+
+
 export NET_TEST_RUNNERS_INSTALL_DIR=/opt;
 tmp="${TMPDIR:-/tmp}"
 archive=https://raw.githubusercontent.com/devizer/glist/master/bin/net-test-runners.tar.gz;
