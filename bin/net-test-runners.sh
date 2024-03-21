efd8296ea0426d8cebef6a7bada26f5f23f6599e
devizer <devizer@users.github.io>
2024-03-21 02:51:40 +0200

    3rd net test runners on termux

--- bin/net-test-runners.sh
+++ bin/net-test-runners.sh
@@ -36,6 +36,7 @@ tmp="${TMPDIR:-/tmp}"
 archive=https://raw.githubusercontent.com/devizer/glist/master/bin/net-test-runners.tar.gz;
 cmd="(wget -O $tmp/net-test-runners.tar.gz --no-check-certificate $archive 2>/dev/null || curl -o $tmp/net-test-runners.tar.gz -kSL $archive)"
 eval "$cmd" || eval "$cmd" || eval "$cmd" || rm -f $tmp/net-test-runners.tar.gz
+sudo mkdir -p "$NET_TEST_RUNNERS_INSTALL_DIR"
 pushd "$NET_TEST_RUNNERS_INSTALL_DIR" >/dev/null
 sudo tar xzf $tmp/net-test-runners.tar.gz
 cd net-test-runners 
