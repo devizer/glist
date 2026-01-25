d75d7cdec85011b497c27f81fab177f9c172c58a
devizer <devizer@github.io>
2026-01-26 01:54:32 +0200

    install-nuget-6.sh

--- bin/install-nuget-6.sh
+++ bin/install-nuget-6.sh
@@ -4,6 +4,9 @@
 f=/usr/local/share/nuget/nuget-latest.exe
 url=https://dist.nuget.org/win-x86-commandline/v3.4.4/nuget.exe
 url=https://dist.nuget.org/win-x86-commandline/latest/nuget.exe
+url=https://dist.nuget.org/win-x86-commandline/v5.11.6/nuget.exe
+url=https://dist.nuget.org/win-x86-commandline/v5.6.0/nuget.exe
+# msbuild 16.6 came with v5.6
 
 sudo mkdir -p "$(dirname "$f")"
 echo "Downloading '$url' as '$f'"
@@ -15,3 +18,7 @@ mono '$f' "$@"
 ' | sudo tee /usr/local/bin/nuget >/dev/null
 sudo chmod +x /usr/local/bin/nuget 
 
+if [[ -n "$(command -v mono)" ]]; then
+  # Show nuget version
+  nuget 2>&1 >/tmp/nuget.ver; cat /tmp/nuget.ver | head -1
+fi
