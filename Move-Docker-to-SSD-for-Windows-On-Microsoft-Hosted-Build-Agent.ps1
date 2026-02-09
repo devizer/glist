ab39b56ff3c727f5d358584178985ec3dd1667ab
devizer <devizer@users.github.io>
2026-02-09 17:46:16 +0200

    Accelerated Move-Docker-to-SSD-for-Windows-On-Microsoft-Hosted-Build-Agent.ps1

--- Move-Docker-to-SSD-for-Windows-On-Microsoft-Hosted-Build-Agent.ps1
+++ Move-Docker-to-SSD-for-Windows-On-Microsoft-Hosted-Build-Agent.ps1
@@ -40,7 +40,7 @@
            $config = Get-Content -Raw -Path $jsonFile | ConvertFrom-Json
            $config | Add-Member -MemberType NoteProperty -Name "data-root" -Value $new_data_root -Force
            $cpuCount=$([Environment]::ProcessorCount)
-           $config | Add-Member -MemberType NoteProperty -Name "cpu-count" -Value $cpuCount -Force
+           # $config | Add-Member -MemberType NoteProperty -Name "cpu-count" -Value $cpuCount -Force
            # $config | Add-Member -MemberType NoteProperty -Name "memory" -Value 4096 -Force
            $new_json = $config | ConvertTo-Json -Depth 10
            [System.IO.File]::WriteAllText($jsonFile, $new_json, (New-Object System.Text.UTF8Encoding($false)))
