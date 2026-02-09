cf0d5c743984639635daa163df8027e843296ce2
devizer <devizer@users.github.io>
2026-02-09 14:23:17 +0200

    Setup-PowerShell-Core-on-Windows.ps1

--- Setup-PowerShell-Core-on-Windows.ps1
+++ Setup-PowerShell-Core-on-Windows.ps1
@@ -1,8 +1,12 @@
 # Run-Remote-Script https://raw.githubusercontent.com/devizer/glist/master/Setup-PowerShell-Core-on-Windows.ps1 -Version 7.4.13 -InstallTo "C:\Apps\PowerShell.Core"
 param($version="7.4.13", $installTo="$($Env:SystemDrive)\Apps\PowerShell.Core")
 
-$url="https://github.com/PowerShell/PowerShell/releases/download/v$($version)/PowerShell-$($version)-win-x64.zip"
-$file="PowerShell-$($version)-win-x64.zip"
+# TODO: ARCHITECTURE
+
+$arch_suffix=Get-CPU-Architecture-Suffix-for-Windows
+
+$url="https://github.com/PowerShell/PowerShell/releases/download/v$($version)/PowerShell-$($version)-win-$($arch_suffix).zip"
+$file="PowerShell-$($version)-win-$($arch_suffix).zip"
 
 pushd "$ENV:USERPROFILE"
 Download-File-Managed "$url" ".\$file"
