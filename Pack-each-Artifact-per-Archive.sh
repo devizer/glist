64dd39db7d7cc4d615196422fd6b2b80b39ed783
devizer <devizer@users.github.io>
2026-02-12 17:30:57 +0200

    Pack-each-Artifact-per-Archive.sh

--- Pack-each-Artifact-per-Archive.sh
+++ Pack-each-Artifact-per-Archive.sh
@@ -3,13 +3,10 @@
 set -eu; set -o pipefail
 COMPRESSION_LEVEL="${COMPRESSION_LEVEL:-6}"
 Say "Pack Artifact Per Archive: [$(pwd -P)]; Compression Level = $COMPRESSION_LEVEL"
-7z || true
-7zz || true
-
 find . -maxdepth 1 -type d -not -path '.' | while read -r folder; do
     echo "Processing folder $folder"
     startAt=$(Get-Global-Seconds)
-    7z a -ms=on -mqs=0 -bd -mx=$COMPRESSION_LEVEL "${folder}.7z" "$folder"
+    7z a -ms=on -mqs=on -bd -mx=$COMPRESSION_LEVEL "${folder}.7z" "$folder"
     seconds=$(( $(Get-Global-Seconds) - startAt ))
     seconds_string="$seconds seconds"; [[ "$seconds" == "1" ]] && seconds_string="1 second"
     Colorize LightGreen "$(Format-Thousand "$(Get-File-Size "${folder}.7z")") bytes (took $seconds_string)"
