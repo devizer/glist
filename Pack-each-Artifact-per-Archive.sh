a8f22ad7ef74d034b6f68d9faf25779491badc66
devizer <devizer@users.github.io>
2026-02-12 18:39:19 +0200

    Pack-each-Artifact-per-Archive.sh

--- Pack-each-Artifact-per-Archive.sh
+++ Pack-each-Artifact-per-Archive.sh
@@ -1,7 +1,37 @@
 #!/usr/bin/env bash
 # Run-Remote-Script https://raw.githubusercontent.com/devizer/glist/master/Pack-each-Artifact-per-Archive.sh
 set -eu; set -o pipefail
+
 COMPRESSION_LEVEL="${COMPRESSION_LEVEL:-6}"
+COMPRESS_TYPE="${COMPRESS_TYPE:-7z}"
+COMPRESS_METHOD="${COMPRESS_METHOD:-lzma2}"
+
+while [[ $# -gt 0 ]]; do
+    case "$1" in
+        # Уровень: -1 ... -9
+        -[1-9]) 
+            COMPRESSION_LEVEL="${1#-}" 
+            ;;
+        # Метод
+        -lzma|-lzma2|-ppmd|-bzip2) 
+            COMPRESS_METHOD="${1#-}" 
+            ;;
+        # Тип
+        -7z|-xz|-zip|-gz) 
+            COMPRESS_TYPE="${1#-}" 
+            ;;
+        *) 
+            echo "Unknown option: $1" >&2
+            ;;
+    esac
+    shift
+done
+
+# Проверка результата
+echo "Level:  $COMPRESSION_LEVEL"
+echo "Type:   $COMPRESS_TYPE"
+echo "Method: $COMPRESS_METHOD"
+
 Say --Reset-Stopwatch
 Say "Pack Artifact Per Archive: [$(pwd -P)]; Compression Level = $COMPRESSION_LEVEL"
 log=$(mktemp)
@@ -12,12 +42,21 @@ index=0
 cat "$list" | while IFS= read -r line; do
     index=$((index+1))
     folder="$(basename "$line")"
-    echo "[$index of $count] Processing folder $folder"
+    echo "[$index of $count] Processing folder '$folder'"
     startAt=$(Get-Global-Seconds)
     err=""
-    7z a -ms=on -mqs=on -bd -mx=$COMPRESSION_LEVEL "${folder}.7z" "$folder" 2>&1 >"$log" || err=err
+    if [[ "$COMPRESS_TYPE" == "7z" ]]; then
+        archive="${folder}.7z"
+        7z a -ms=on -mqs=on -bd -m0=$COMPRESS_METHOD -mx=$COMPRESSION_LEVEL "$archive" "$folder" 2>&1 >"$log" || err=err
+    elif [[ "$COMPRESS_TYPE" == "xz" ]]; then
+        archive="${folder}.tar.xz"
+        tar cf - "$folder" | 7z a dummy -txz -mx=$COMPRESSION_LEVEL -si -so > "$archive" || err=err
+    else
+       Colorize Red "Compression Type '$COMPRESS_TYPE' is unknown"
+       return 2
+    fi
     if [[ -n "$err" ]]; then
-      Colorize Red "7-zip compression failed"
+      Colorize Red "$COMPRESS_TYPE compression failed"
       cat "$log"
       rm -f "$log" 2>/dev/null || true
       return 1
@@ -25,7 +64,7 @@ cat "$list" | while IFS= read -r line; do
     rm -f "$log" 2>/dev/null || true
     seconds=$(( $(Get-Global-Seconds) - startAt ))
     seconds_string="$seconds seconds"; [[ "$seconds" == "1" ]] && seconds_string="1 second"
-    Colorize LightGreen "OK. ${folder}.7z is $(Format-Thousand "$(Get-File-Size "${folder}.7z")") bytes (took $seconds_string)"
+    Colorize LightGreen "OK. "$archive" is $(Format-Thousand "$(Get-File-Size "$archive")") bytes (took $seconds_string)"
     rm -rf "$folder" 2>/dev/null || rm -rf "$folder" 2>/dev/null || rm -rf "$folder"
 done
 Say "Done: $count folder(s) are compressed"
